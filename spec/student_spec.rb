ee7d2bb1bb182551fc10b5750dd1fda6a32807e5
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-07-06 15:04:28 -0600

    Add a working cookie class &  test file

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,13 +1,30 @@
-# Add the 2 requires you will need here
-
+require './lib/student'
 
 describe Student do
-  describe '#initialize' do
-    it 'is an instance of student' do
-      # write the code to initialize a new student object
-    end
-    # test it has a name
-    # test it has cookies
-    # test it can add cookies
+  it 'it exists' do
+    student = Student.new('Penelope')
+
+    expect(student).to be_an_instance_of(Student)
   end
+
+  it 'has a name' do
+    student = Student.new('Penelope')
+
+    expect(student.name).to eq('Penelope')
+  end
+
+  it 'has empty cookies to start' do
+    student = Student.new('Penelope')
+
+    expect(student.cookies).to eq([])
+  end
+
+  it 'can add cookies' do
+    student = Student.new('Penelope')
+    student.add_cookie('Chocolate Chunk')
+    student.add_cookie('Snickerdoodle')
+
+    expect(student.cookies).to eq(["Chocolate Chunk", "Snickerdoodle"])
+  end
+
 end
